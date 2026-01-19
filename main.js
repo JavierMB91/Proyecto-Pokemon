50fcf5ceab2a013f76d2cfefe4eb4be1e9f2ceff
Javier Morilla Barrena <jmorillabarrena@gmail.com>
2026-01-19 22:52:18 +0100

    Actualizaciones automaticas

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const path = require('path');
 
 function createWindow() {
@@ -9,7 +10,7 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
-            webSecurity: false // IMPORTANTE: Permite cargar nav.html y footer.html localmente
+            webSecurity: false
         }
     });
 
@@ -23,6 +24,11 @@ function createWindow() {
 
 app.whenReady().then(() => {
     createWindow();
+    
+    // Buscar actualizaciones automáticamente al iniciar (solo en producción)
+    if (app.isPackaged) {
+        autoUpdater.checkForUpdatesAndNotify();
+    }
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
@@ -35,4 +41,19 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
+});
+
+// --- EVENTOS DEL AUTO-UPDATER ---
+
+autoUpdater.on('update-downloaded', (info) => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Actualización lista',
+        message: `La versión ${info.version} se ha descargado. ¿Quieres reiniciar ahora para instalarla?`,
+        buttons: ['Reiniciar', 'Más tarde']
+    }).then((result) => {
+        if (result.response === 0) { // Si pulsa "Reiniciar"
+            autoUpdater.quitAndInstall();
+        }
+    });
 });
